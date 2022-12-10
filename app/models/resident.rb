cb7de191eef5dbf0460d57574a1bd9ca4c7ecde5
Joe King <j.king92@gmail.com>
2022-12-10 16:20:54 -0700

    Add average age to residents index page

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -5,4 +5,7 @@ class Resident < ApplicationRecord
 
   has_many :resident_courses
   has_many :courses, through: :resident_courses
+  def self.average_age
+    average(:age).to_f.round(1)
+  end
 end
--- spec/models/resident_spec.rb
+++ spec/models/resident_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe Resident, type: :model do
     it {should have_many(:courses).through(:resident_courses)}
   end
 
+  describe '#average_age' do
+    it 'returns average age of all residents to the tenths place' do
+      resident1 = Resident.create!(name: "Jessica Rabbit", age: 68, occupation: "Singer")
+      resident2 = Resident.create!(name: "Seth Rogan", age: 43, occupation: "Comedian")
+      resident3 = Resident.create!(name: "Miles Morales", age: 18, occupation: "Student/Spiderman")
+
+      expect(Resident.average_age).to eq(43)
+
+      resident4 = Resident.create!(name: "Bruce Chutback", age: 16, occupation: "Student/New Guy In Town")
+
+      expect(Resident.average_age).to eq(36.3)
+    end
+  end
 end
